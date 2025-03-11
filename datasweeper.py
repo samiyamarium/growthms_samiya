ecd960b68c12d0188c6deabfa582f26df4b19fed
samiyamarium <160397803+samiyamarium@users.noreply.github.com>
2025-03-12 01:54:54 +0500

    Update datasweeper.py

--- datasweeper.py
+++ datasweeper.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 from io import BytesIO
 import os
-import openpyxl
+
 
 st.set_page_config(" Data sweeper by samiya marium",layout="wide",page_icon=":rosette:")
 st.title(" :rosette: Data Sweeper by samiya marium:rosette: ")
